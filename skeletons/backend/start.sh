c8ab27f677ffb98da3981b0407637f190bb85fbd
chatt <nodeadbatteries@gmail.com>
2026-01-06 16:59:05 -0600

    feat: allow server heap space configuration

--- skeletons/backend/start.sh
+++ skeletons/backend/start.sh
@@ -10,7 +10,14 @@ if [ -f .env ]; then
   set +a
 fi
 
-java -Xms1536M -Xmx1536M -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem \
+BRADY_INIT_HEAP_SPACE=${BRADY_INIT_HEAP_SPACE:-1536M}
+if [[ "$BRADY_ENABLE_NMT" == "true" ]]; then
+    BRADY_ENABLE_NMT_FLAG="-XX:NativeMemoryTracking=summary"
+else
+    BRADY_ENABLE_NMT_FLAG=""
+fi
+
+java -Xms${BRADY_INIT_HEAP_SPACE} -Xmx${BRADY_INIT_HEAP_SPACE} ${BRADY_ENABLE_NMT_FLAG} -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem \
  -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 \
  -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 \
  -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 \
