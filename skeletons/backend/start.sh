d8aed75bb20b435438774dbd32e12c43ff1d96a5
imkunet <im@kunet.dev>
2026-01-31 19:07:02 -0500

    fix: permissive flag

--- skeletons/backend/start.sh
+++ skeletons/backend/start.sh
@@ -17,10 +17,11 @@ else
     BRADY_ENABLE_NMT_FLAG=""
 fi
 
-java -Xms${BRADY_INIT_HEAP_SPACE} -Xmx${BRADY_INIT_HEAP_SPACE} ${BRADY_ENABLE_NMT_FLAG} -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem \
+java -Xms${BRADY_INIT_HEAP_SPACE} -Xmx${BRADY_INIT_HEAP_SPACE} ${BRADY_ENABLE_NMT_FLAG} \
+ -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem \
  -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 \
  -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 \
  -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 \
  -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Dfile.encoding=UTF-8 \
  --add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED \
- --add-opens=java.base/java.lang.invoke=ALL-UNNAMED -jar sportpaper-1.8.8.jar nogui
+ --add-opens=java.base/java.lang.invoke=ALL-UNNAMED --enable-native-access=ALL-UNNAMED -jar sportpaper-1.8.8.jar nogui
