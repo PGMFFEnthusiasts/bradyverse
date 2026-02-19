03a023829df45b5b977cc1ff59d92a9dbf41c490
imkunet <im@kunet.dev>
2026-02-19 10:52:54 -0500

    feat: z

--- skeletons/backend/start.sh
+++ skeletons/backend/start.sh
@@ -17,11 +17,11 @@ else
     BRADY_ENABLE_NMT_FLAG=""
 fi
 
+# shoutout https://exa.y2k.diy/garden/jvm-args/ and mbt for linking me ts
 java -Xms${BRADY_INIT_HEAP_SPACE} -Xmx${BRADY_INIT_HEAP_SPACE} ${BRADY_ENABLE_NMT_FLAG} \
- -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem \
- -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 \
- -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 \
- -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 \
- -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Dfile.encoding=UTF-8 \
- --add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED \
- --add-opens=java.base/java.lang.invoke=ALL-UNNAMED --enable-native-access=ALL-UNNAMED -jar sportpaper-1.8.8.jar nogui
+  -XX:+UseZGC -XX:+UseCompactObjectHeaders \
+  -XX:+DisableExplicitGC \
+  -Dfile.encoding=UTF-8 \
+  --add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED \
+  --add-opens=java.base/java.lang.invoke=ALL-UNNAMED --enable-native-access=ALL-UNNAMED \
+  -jar sportpaper-1.8.8.jar nogui
